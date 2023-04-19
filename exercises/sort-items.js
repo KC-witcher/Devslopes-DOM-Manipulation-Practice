c0d9d66bd443712dec68fcad459a0632721c6c3b
Shubham Kc <shubhamkc1997@gmail.com>
2023-04-19 19:49:44 -0400

    Finished Sort Item Section

--- exercises/sort-items.js
+++ exercises/sort-items.js
@@ -1,8 +1,8 @@
 /**
  * SORTING NODES WITHIN A CONTAINER
  * Please, make sure to read the following files in the exercises-info folder before you start
- * * "02 SortingNode.md" 
-*/
+ * * "02 SortingNode.md"
+ */
 
 /**
  * @task
@@ -12,8 +12,7 @@
  */
 
 // Your code goes here...
-
-
+const allItems = document.querySelectorAll(".item");
 
 /**
  * @task
@@ -23,8 +22,7 @@
  */
 
 // Your code goes here...
-
-
+const sortBtn = document.querySelectorAll(".sortBtn");
 
 /**
  * @task
@@ -37,9 +35,29 @@
  * Example: sortData('asc') => <a-z order of items in the main container>
  */
 
-// Your code goes here...
-
-
+// Your code goes here... 8667186991
+function sortData(direction) {
+  const mainNode = document.getElementById("main");
+  const nodeArray = Array.from(allItems);
+  nodeArray.sort((a, b) => {
+    if (direction == "asc") {
+      if (a.id > b.id) {
+        return 1;
+      } else if (a.id < b.id) {
+        return -1;
+      } else return 0;
+    } else if (direction == "desc") {
+      if (a.id < b.id) {
+        return 1;
+      } else if (a.id > b.id) {
+        return -1;
+      } else return 0;
+    }
+  });
+  nodeArray.forEach((node) => {
+    mainNode.append(node);
+  });
+}
 
 /**
  * @task
@@ -50,5 +68,8 @@
  */
 
 // Your code goes here...
-
-
+sortBtn.forEach((node) => {
+  node.addEventListener("click", () => {
+    sortData(node.getAttribute("data-sortdir"));
+  });
+});
